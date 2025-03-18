909a1dde6097e5ab3e30253d442159e5680e34b2
rakaso598 <nexxus86@outlook.com>
2025-03-18 16:19:36 +0900

    헬스체크모듈 업데이트

--- src/modules/healthCheck.module.js
+++ src/modules/healthCheck.module.js
@@ -9,6 +9,7 @@ const healthCheckRouter = express.Router();
 healthCheckRouter.get("/", (req, res, next) => {
   try {
     console.log("OK");
+    res.status(200).send("헬스체크 성공!");
   } catch (e) {
     next(e);
   }
